3882bb2f9d63be6823b13ac6c81c760d871e6904
kostya shilkrot <kostya.shilkrot@gmail.com>
2026-03-17 16:34:36 +0200

    Refactor async functions and enhance Person class: renamed async_func to async_func1 and added async_func2 for better clarity. Introduced func_with_args method to Person class for handling variable arguments. Added Peope class extending Person with a custom string representation.

--- async/main.py
+++ async/main.py
@@ -7,29 +7,36 @@ def sync_func(test_param: str) -> str:
     return f"Sync function result: {test_param}"
 
 
-async def async_func(test_param: str) -> str:
+async def async_func1(test_param: str) -> str:
     await asyncio.sleep(1)
     return f"Async function result: {test_param}"
 
 
-async def my_func() -> None:
+async def async_func2(test_param: str) -> None:
     await asyncio.sleep(1)
-    print("My function is r unning")
+    return f"Async function result: {test_param}"
 
 
 async def main():
 
-    result = await asyncio.create_task(my_func())
-    print(result)
+    # result = await asyncio.create_task(async_func2())
+    # print(result)
 
+    # start_time = time.time()
+    # sync_result = sync_func("Hello")
+    # async_result = await async_func1("Hello")
+    # end_time = time.time()
+    # print(f"Sync function took {end_time - start_time} seconds")
+    # print(f"Async function took {end_time - start_time} seconds")
+    # print(sync_result)
+    # print(async_result)
     start_time = time.time()
-    sync_result = sync_func("Hello")
-    async_result = await async_func("Hello")
+    task1 = asyncio.create_task(async_func1("Hello"))
+    task2 = asyncio.create_task(async_func2("Kostya is a good boy"))
+    result = await asyncio.gather(task1, task2)
     end_time = time.time()
-    print(f"Sync function took {end_time - start_time} seconds")
-    print(f"Async function took {end_time - start_time} seconds")
-    print(sync_result)
-    print(async_result)
+    print(result)
+    print(f"Time taken: {end_time - start_time} seconds")
 
 
 if __name__ == "__main__":
--- classes_methods/main.py
+++ classes_methods/main.py
@@ -20,6 +20,10 @@ class Person:
         age += years
         return age
 
+    def func_with_args(*args, **kwargs) -> None:
+        print(args)
+        print(kwargs)
+
 
 person = Person(name="John", age=30, email="john@example.com")
 person.say_hello()
@@ -30,3 +34,20 @@ person2.say_hello()
 
 res = Person.add_age(10, person.age)
 print(f"The new age is {res}")
+
+
+person.func_with_args(
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, name="John", age=30, email="john@example.com"
+)
+
+
+@dataclass
+class Peope(Person):
+    country: str
+
+    def __str__(self) -> str:
+        return f"Person(name={self.name}, age={self.age}, email={self.email}, country={self.country})"
+
+
+peope = Peope(name="John", age=30, email="john@example.com", country="Israel")
+print(peope)
